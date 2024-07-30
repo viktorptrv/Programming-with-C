db803d35225c6daaefa9d2b13d7520f1b94b9693
Viktor Petrov <115278686+viktorptrv@users.noreply.github.com>
2024-07-30 22:45:09 +0300

    Update ContactMenaging.c

--- ContactMenaging.c
+++ ContactMenaging.c
@@ -35,35 +35,34 @@ int main(void) {
 
 	printf_s("This is a Contact Managing System.\nDo you want to proceed? y/n: ");
 	ch = getchar();
+	while (1) {
+		if (ch == 'y') {
 
-	if (ch == 'y') {
+			printf_s("\nWhat do you want to print?\n");
+			printf_s("ID - 1; Phones - 2; Age - 3 -> ");
 
-		printf_s("\nWhat do you want to print?\n");
-		printf_s("ID - 1; Phones - 2; Age - 3 -> ");
+			scanf_s("%d", &var);
 
-		scanf_s("%d", &var);
-
-		switch (var) {
-		case 1:
-			print_id();
-			break;
-		case 2:
-			print_phones();
-			break;
-		case 3:
-			print_age();
-			break;
-		default:
-			printf_s("There was a mistake!\n");
-			break;
+			switch (var) {
+			case 1:
+				print_id();
+				break;
+			case 2:
+				print_phones();
+				break;
+			case 3:
+				print_age();
+				break;
+			default:
+				printf_s("There was a mistake!\n");
+				break;
+			}
 		}
-	}
 
-	printf_s("Do you want to proceed with manipulations? y/n: ");
-	scanf_s(" %c", &ch);
-	
-	if (ch == 'y') {
-		while (ch != 'n') {
+		printf_s("Do you want to proceed with manipulations? y/n: ");
+		scanf_s(" %c", &ch);
+
+		if (ch == 'y') {
 			print_actions();
 			scanf_s(" %c", &ch);
 
@@ -87,35 +86,68 @@ int main(void) {
 				break;
 			}
 		}
-	}
 
+		printf_s("\nContinue? y/n");
+		scanf_s(" %c", &ch);
+		if (character == 'n')
+			break;
+	}
 	printf_s("\nBye bye...\n");
 
 	return 0;
 }
 
 void change_id(void) {
+	int new_id;
+	int id_old;
 
+	printf_s("Which id do you want to change? ");
+	scanf_s("%d", &id_old);
+
+	if (id[id_old]) {
+		printf_s("\nWhat will be the new id? ");
+		scanf_s("%d", &new_id);
+		id[id_old] = new_id;
+	}
 }
 
 void change_age(void) {
-
+	int id_of_age, new_age;
+	printf_s("\nWhich id and to what age to be changed?");
+	scanf_s("%d %d", &id_of_age, &new_age);
+	if (age[id_of_age]) {
+		age[id_of_age] = new_age;
+	}
 }
 
 void change_number(void) {
+	int id_of_num;
+	long long int num;
 
+	printf_s("\nWhich id and to what number to be changed?");
+	scanf_s("%d %llu", &id_of_num, &num);
+	if (phone[id_of_num]) {
+		phone[id_of_num] = num;
+	}
 }
 
 void block_id(void) {
-
+	int id_to_block;
+	printf_s("\nWhich id to block");
+	scanf_s("%d", &id_to_block);
+	if (age[id_to_block] && phone[id_to_block] && id[id_to_block]) {
+		age[id_to_block] = 0;
+		id[id_to_block] = 0;
+		phone[id_to_block] = 0;
+	}
 }
 
 void print_actions(void) {
-	printf_s("Possible Actions are:\na) Delete an ID - you will also delete the corresponding phones and ages\n");
-	printf_s("b) Change an ID\n");
-	printf_s("c) Change Age\n");
-	printf_s("d) Change phone number\n");
-	printf_s("e) Block ID\nChoose action: ");
+	printf_s("Possible Actions are:\n");
+	printf_s("a) Change an ID\n");
+	printf_s("b) Change Age\n");
+	printf_s("c) Change phone number\n");
+	printf_s("d) Block ID\nChoose action: ");
 }
 
 void print_id(void) {
