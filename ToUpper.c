4a0a4b926c607f62e796e16abe95cb5a9dac39f9
Viktor Petrov <115278686+viktorptrv@users.noreply.github.com>
2024-08-04 22:40:52 +0300

    Update ToUpper.c

--- ToUpper.c
+++ ToUpper.c
@@ -30,3 +30,40 @@ void capitalize(char* arr) {
     }
     printf("New string: %s\n", arr);
 }
+
+// with pointers
+#include <stdio.h>
+#include <ctype.h>
+
+#define Num 10
+
+void capitalize(char* arr);
+
+char arr_string[Num];
+
+int main(void) {
+    printf("Enter a string: ");
+
+    if (fgets(arr_string, Num, stdin)) {
+        size_t len = strlen(arr_string);
+        if (len > 0 && arr_string[len - 1] == '\n') {
+            arr_string[len - 1] = '\0';
+        }
+    }
+
+    capitalize(&arr_string[0]);
+
+    fputs(arr_string, stdout);
+
+    return 0;
+}
+
+void capitalize(char* arr) {
+    for (int i = 0; i < Num; i++) {
+        if (isalpha(*arr)) {
+            *arr = toupper(*arr);
+        }
+        arr++;
+    }
+    printf("New string:");
+}
